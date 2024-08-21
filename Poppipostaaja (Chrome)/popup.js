f4256cad5b10045dbe3df6212fe031f6ca31af42
Daniel Kristiansson <daniel.i.kristiansson@gmail.com>
2024-08-21 03:36:51 +0300

    Get tab url instead of extension url

--- Poppipostaaja (Chrome)/popup.js
+++ Poppipostaaja (Chrome)/popup.js
@@ -6,9 +6,6 @@ function getCookie(name, callback) {
     });
 }
 
-const videoUrl = window.location.href.split("&")[0] || window.location.href;
-const videoId = videoUrl.split("v=")[1];
-
 document.addEventListener('DOMContentLoaded', function () {
     // Check if there's a stored post ID and set it as the input's value
     const postIdInput = document.getElementById('postId');
@@ -23,30 +20,36 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     const postId = document.getElementById('postId').value;
-    if (postId === "") {
-        document.getElementById('postButton').disabled = true;
-        document.getElementById('postButton').innerText = "Syötä postin ID ja avaa extension uudelleen";
-    } else {
-        fetch("https://ranssi.paivola.fi/post/" + postId, {
-            "headers": {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "accept-language": "en-US,en;q=0.9,fi;q=0.8,de;q=0.7",
-                "cookie": `__Host-next-auth.csrf-token=${getCookie("_Host-next-auth.csrf-token")}; __Secure-next-auth.session-token=${getCookie("__Secure-next-auth.session")}`,
-            },
-            "body": null,
-            "method": "GET"
-        }).then(response => response.text())
-            .then(text => {
-                if (text.includes(videoId)) {
-                    document.getElementById('postButton').disabled = true;
-                    document.getElementById('postButton').innerText = "BIISI ON JO POSTATTU!";
-                } else {
-                    document.getElementById('postButton').disabled = false;
-                    document.getElementById('postButton').innerText = "Postaa poppeihin";
-                }
 
-            });
-    }
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0];
+        const videoUrl = activeTab.url.split("&")[0] || activeTab.url;
+        const videoId = videoUrl.split("v=")[1];
+        if (postId === "") {
+            document.getElementById('postButton').disabled = true;
+            document.getElementById('postButton').innerText = "Syötä postauksen ID ja avaa extension uudelleen";
+        } else {
+            fetch("https://ranssi.paivola.fi/post/" + postId, {
+                "headers": {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "accept-language": "en-US,en;q=0.9,fi;q=0.8,de;q=0.7",
+                    "cookie": `__Host-next-auth.csrf-token=${getCookie("_Host-next-auth.csrf-token")}; __Secure-next-auth.session-token=${getCookie("__Secure-next-auth.session")}`,
+                },
+                "body": null,
+                "method": "GET"
+            }).then(response => response.text())
+                .then(text => {
+                    if (text.includes(videoId)) {
+                        document.getElementById('postButton').disabled = true;
+                        document.getElementById('postButton').innerText = "BIISI ON JO POSTATTU!";
+                    } else {
+                        document.getElementById('postButton').disabled = false;
+                        document.getElementById('postButton').innerText = "Postaa poppeihin";
+                    }
+
+                });
+        }
+    });
 });
 
 document.getElementById('postButton').addEventListener('click', async () => {
